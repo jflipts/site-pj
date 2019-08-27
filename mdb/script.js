34b4c3e76595a567c278c769f97930298495c21e
jflipts <Jeroen.Flipts@UGent.be>
2019-08-27 22:33:29 +0200

    Expanding bio and urls

---
+++ mdb/script.js
@@ -0,0 +1,9 @@
+// Function that collapses the button of the bio collapser
+function collapseJS() {
+    var x = document.getElementById("collapseTopButton");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
