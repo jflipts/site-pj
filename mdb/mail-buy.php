be9932def14c89cd1711889f09e46d534ef6fd42
Jeroen Flipts <jeroen.flipts@gmail.com>
2020-04-13 17:32:39 +0200

    Rework buy form

--- mdb/mail-buy.php
+++ mdb/mail-buy.php
@@ -1,39 +1,79 @@
 <?php 
-$cdData = $_POST['formCD'];
 $name = $_POST['formName'];
 $email = $_POST['formEmail'];
 $phone = $_POST['formPhone'];
-$message = $_POST['formAddress'];
+$address = $_POST['formAddress'];
+$cdData = $_POST['formCD'];
 
 header('Content-Type: application/json');
 if ($name === ''){
-    print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+    print json_encode(array('message' => 'Error: Name cannot be empty', 'code' => 0));
     exit();
 }
 if ($email === ''){
-    print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+    print json_encode(array('message' => 'Error: Email cannot be empty', 'code' => 0));
     exit();
 } else {
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
+        print json_encode(array('message' => 'Error: Email format invalid.', 'code' => 0));
         exit();
     }
 }
 if ($phone === ''){
-    print json_encode(array('message' => 'Phone cannot be empty', 'code' => 0));
+    print json_encode(array('message' => 'Error: Phone cannot be empty', 'code' => 0));
     exit();
 }
-if ($message === ''){
-    print json_encode(array('message' => 'Description cannot be empty', 'code' => 0));
+if ($address === ''){
+    print json_encode(array('message' => 'Error: Description cannot be empty', 'code' => 0));
     exit();
+}
+if (count($cdData) < 1) {
+    print json_encode(array('message' => 'Error: Order at least one item', 'code' => 0));
+    exit();
+}
+$cdDataString = "";
+foreach ($cdData as $key => $value) {
+    $cdDataString .= "\tItem: " . $value['name'] . " -> Amount: " . $value['amount'] . "\n";
 }
 
+// Mail to PJ
 $subject = "Order Request $name";
-
-$content="From: $name \n Email: $email \n Phone: $phone \n Message: $message \n Order: $cdData";
+$content = "
+From: $name
+Email: $email
+Phone: $phone
+Address: $address
+Order: 
+$cdDataString
+";
 $recipient = "obc.artistsmanagement@gmail.com";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array('message' => 'Email successfully sent! We will get in touch with you shortly.', 'code' => 1));
+mail($recipient, $subject, wordwrap($content), $mailheader) or die("Error!");
+
+// Mail to client
+$subject = "Thank you for your order at pieterjanverhoyen.be";
+$content = "Dear $name
+
+Thank you for your order at pieterjanverhoyen.be, you can review your order details below. In case there would be any errors in these order details, please reply to this email.
+Payment details will be sent to you within 5 business days. The order will be shipped after payment has been received.
+
+Order details: 
+Name: $name
+Email: $email
+Phone: $phone
+Address: $address
+Order: 
+$cdDataString
+
+
+Kind regards
+
+Pieter-Jan Verhoyen
+";
+$recipient = $email;
+$mailheader = "From: obc.artistsmanagement@gmail.com \r\n";
+mail($recipient, $subject, wordwrap($content), $mailheader) or die("Error!");
+
+print json_encode(array('message' => 'Order successful! We will get in touch with you shortly. A Confirmation Email has been sent to you. Don’t forget to check your junk tab.', 'code' => 1));
 exit();
 ?>
--- mdb/script.js
+++ mdb/script.js
@@ -1,76 +1,33 @@
 // Collapse the hamburger menu on mobile on click
-$('.navbar-nav>li>a').on('click', function(){
+$('.navbar-nav>li>a').on('click', function () {
   $('.navbar-collapse').collapse('hide');
 });
 
 // Function that collapses the button of the bio
 function collapseJS() {
-    var x = document.getElementById("collapseTopButton");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("collapseTopButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
   }
+}
 
 // Function to validate the booking form
 function validateForm() {
   document.getElementById('formStatus').innerHTML = "Sending...";
 
   formData = {
-    'formName'     : $('input[name=formName]').val(),
-    'formEmail'    : $('input[name=formEmail]').val(),
-    'formOrg'  : $('input[name=formOrg]').val(),
-    'formPhone'  : $('input[name=formPhone]').val(),
-    'formDescription'  : $('textarea[name=formDescription]').val()
-    };
-    
-
-  $.ajax({
-    url : "mail.php",
-    type : "POST",
-    data : formData,
-    success : function(data, textStatus, jqXHR)
-    {
-      $('#formStatus').text(data.message);
-      if (data.code) //If mail was sent successfully, reset the form.
-      {
-        $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-        $('#contact-form').closest('form').find("input[type=email], textarea").val("");
-        $('#contact-form').closest('form').find("input[type=tel], textarea").val("");
-        $('#contact-form').closest('form').find(':input').each(function(ix,el)
-        {
-          el.focus();
-        });
-        document.activeElement.blur();
-      }
-    },
-    error: function (errorThrown, textStatus, jqXHR)
-    {
-      $('#formStatus').text(errorThrown);
-    }
-  });
-}
-
-function validateBuyForm() {
-  document.getElementById('formStatus').innerHTML = "Sending...";
-
-  var cdData = [];
-  $.each($("input[name='cd']:checked"), function () {
-    cdData.push($(this).val());
-  });
-
-  formData = {
-    'formCD': cdData.join(", "),
     'formName': $('input[name=formName]').val(),
     'formEmail': $('input[name=formEmail]').val(),
+    'formOrg': $('input[name=formOrg]').val(),
     'formPhone': $('input[name=formPhone]').val(),
     'formDescription': $('textarea[name=formDescription]').val()
   };
 
 
   $.ajax({
-    url: "mail-buy.php",
+    url: "mail.php",
     type: "POST",
     data: formData,
     success: function (data, textStatus, jqXHR) {
@@ -91,3 +48,65 @@ function validateBuyForm() {
     }
   });
 }
+
+$(document).ready(function () {
+
+  // Modal could have been hidden
+  $(".buy-form-btn-open").on('click', function (event) {
+    //event.stopPropagation();
+    //event.stopImmediatePropagation();
+    $('#buyFormBody').show();
+    $('#buy-form-btn-submit').show();
+    $('#buyFormStatus').text("");
+  });
+
+  $("#buy-form-btn-submit").on('click', function (event) {
+    var form = $('#buy-form') 
+    if($('#buy-form')[0].checkValidity() === false) {
+      return true;
+    }
+    event.preventDefault();
+    document.getElementById('buyFormStatus').innerHTML = "Sending...";
+
+    var cdData = [];
+    $.each($("input[name='buy_cd']"), function () {
+      var amount = $(this).val();
+      var label = $("label[for='" + $(this).attr('id') + "']").text();
+      if (amount > 0) {
+        var cd = {
+          name: label,
+          amount: amount,
+        }
+        cdData.push(cd)
+      }
+    });
+
+    if (cdData.length < 1) {
+      $('#buyFormStatus').text("Error: Order at least one item");
+      return
+    }
+
+    var formData = {
+      'formName': $('input[name=buy_name]').val(),
+      'formEmail': $('input[name=buy_email]').val(),
+      'formPhone': $('input[name=buy_phone]').val(),
+      'formAddress': $('input[name=buy_address]').val(),
+      'formCD': cdData,
+    };
+
+    $.ajax({
+      url: "mail-buy.php",
+      type: "POST",
+      data: formData,
+    }).done(function (response, textStatus, jqXHR) {
+      if (response.code) {
+        $('#buyFormBody').hide();
+        $('#buy-form-btn-submit').hide();
+      }
+      $('#buyFormStatus').text(response.message);
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+      var errr = errorThrown
+      $('#buyFormStatus').text(textStatus);
+    });
+  });
+});
